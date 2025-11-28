2091ee303486385d533aec8183efd9620f23d70f
DONCHAN <arpanpatra800188500@gmail.com>
2025-11-28 12:21:20 +0530

    vercel deploy set 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header.jsx";
-import Footer from "./Components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 const productCategories = [
   "Protein Wafer Bar",
