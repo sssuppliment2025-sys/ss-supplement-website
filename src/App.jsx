33da51f85210adcb931861d7bd2b7035831ad4ae
DONCHAN <arpanpatra800188500@gmail.com>
2025-12-01 21:09:24 +0530

    sjhkjashf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
 import Body from "./components/Body.jsx";
 const productCategories = [
   "Protein Wafer Bar",
